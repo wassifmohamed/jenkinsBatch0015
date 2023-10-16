244086ec1cdd802532cd96f45163e507f64613eb
wassifmohamed <wassifmohamed@gmail.com>
2023-10-16 11:24:28 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
+
+
+
+
+
+
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.out.println("Hello ME");
 
         for (int i=0; i<10; i++){
-            System.out.println(i);
+          //  System.out.println(i);
 
-            System.out.println("hello  geryt tryb y qwatny ");
+            System.out.println("hello  everyOne welcome to myspace ");
+            Thread.sleep(2000);
 
         }
     }
